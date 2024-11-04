5f03b8e5351fd6ad25836aa2ca373ea814421123
ejavierds <134799939+ejavierds@users.noreply.github.com>
2024-11-04 17:14:23 -0400

    Update vip_electronic.sql

--- vip_electronic.sql
+++ vip_electronic.sql
@@ -1,22 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 04-11-2024 a las 11:32:00
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Base de datos: `vip_electronic`
 --
@@ -309,7 +290,3 @@ ALTER TABLE `reporte`
   ADD CONSTRAINT `reporte_ibfk_2` FOREIGN KEY (`empleado`) REFERENCES `empleado` (`tipo_documento`),
   ADD CONSTRAINT `reporte_ibfk_3` FOREIGN KEY (`servicio`) REFERENCES `servicio` (`num_asignacion`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
